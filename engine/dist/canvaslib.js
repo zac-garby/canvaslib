9317be52e02138a5bc47ef5db5e88c389a9ecf10
Zac Garby <zac.garby3@gmail.com>
2016-07-20 22:16:17 +0100

    Added argument checking for maths functions

--- engine/dist/canvaslib.js
+++ engine/dist/canvaslib.js
@@ -89,18 +89,22 @@ clib.loadImages = function(sources, finishCallback, singleCallback = (img) => un
 clib.Maths = {};
 
 clib.Maths.clamp = function(x, min, max) {
+    check(3, 3, Number, Number, Number);
     return x < min ? min : x > max ? max : x;
 };
 
 clib.Maths.radToDeg = function(rad) {
+    check(1, 1, Number);
     return rad * (180 / Math.PI);
 };
 
 clib.Maths.degToRad = function(deg) {
+    check(1, 1, Number);
     return deg * (Math.PI / 180);
 };
 
 clib.Maths.lerp = function(a, b, t, allowExtrapolation = false) {
+    check(3, 4, Number, Number, Number, Boolean);
     if (!allowExtrapolation) {
         t = clib.Math.clamp(t, 0, 1);
     }
@@ -583,6 +587,9 @@ function normalizeVec (vec) {
 }
 
 function lerpVec (vec, targetVec, t) {
+    check(3, 3, Object, Object, Number);
+    validateVector(vec, true);
+    validateVector(targetVec, true);
     return {
         x: clib.Maths.lerp(vec.x, targetVec.x, t),
         y: clib.Maths.lerp(vec.y, targetVec.y, t)
--- engine/src/js/vector-maths.js
+++ engine/src/js/vector-maths.js
@@ -58,6 +58,9 @@ function normalizeVec (vec) {
 }
 
 function lerpVec (vec, targetVec, t) {
+    check(3, 3, Object, Object, Number);
+    validateVector(vec, true);
+    validateVector(targetVec, true);
     return {
         x: clib.Maths.lerp(vec.x, targetVec.x, t),
         y: clib.Maths.lerp(vec.y, targetVec.y, t)
