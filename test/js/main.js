2e70ac82410949f126a12415add783355059be20
Zac Garby <zac.garby3@gmail.com>
2016-07-25 11:25:20 +0100

    Added initial support for drawing text

--- test/js/main.js
+++ test/js/main.js
@@ -1,26 +1,7 @@
 var stage;
 var images;
 var imageSources = {};
-
-var menu = new clib.Scene('menu', {
-    render: function(stage, dt) {
-        if (stage.keys[68]) stage.setActiveScene('play');
-        stage.rect(50, 50, 800, 500).fill().closePath();
-    }
-});
-
-var play = new clib.Scene('play', {
-    init: function(stage, dt) {
-        this.x = 0;
-    },
-    update: function(stage, dt) {
-        this.x += 100 * dt;
-    },
-    render: function(stage, dt) {
-        if (stage.keys[65]) stage.setActiveScene('menu');
-        stage.circle(this.x, 300, 100).fill().closePath();
-    }
-});
+var text = prompt('Text:');
 
 function load() {
     clib.loadImages(imageSources, function(imgs) { // Load the images
@@ -32,20 +13,21 @@ function load() {
 function init() {
     stage = new clib.Stage('canvas', {}); // Create the stage
 
-    stage.addScenes(menu, play);
-    stage.setActiveScene('menu');
-
     stage.on('tick', function() { // Call 'tick()' every stage update
         tick();
     });
 }
 
 function tick() {
-    stage.updateScene();
     render(); // Render stuff
 }
 
 function render() {
     stage.clear();
-    stage.renderScene();
+    stage.strokeText(text, stage.getDimensions().width / 2, stage.getDimensions().height / 2, {
+        font: '50px cursive',
+        style: 'green',
+        baseline: 'middle',
+        align: 'center'
+    });
 }
